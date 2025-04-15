06ebcb3d65f5d7a64f3ed1efd7e3967ee1517dba
Josh Birch <joshuabirch@protonmail.com>
2025-04-15 07:37:08 +0100

    Start problem 001, unfinished

---
+++ src/Problem001.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem001
+{
+    public static void main(String[] args)
+    {
+          /* EXAMPLE */
+//        int[] listOne = new int[]{3, 4, 2, 1, 3, 3};
+//        int[] listTwo = new int[]{4, 3, 5, 3, 9, 3};
+//
+//        Arrays.sort(listOne);
+//        Arrays.sort(listTwo);
+//
+//        int distance = 0;
+//
+//        for (int i = 0; i < listOne.length; i++)
+//        {
+//            distance += (listTwo[i] - listOne[i]);
+//        }
+//
+//        System.out.println(distance);
+
+        File numberFile = new File("problem001.dat");
+
+        try
+        {
+            Scanner fileScanner = new Scanner(numberFile);
+
+
+        }
+    }
+}
