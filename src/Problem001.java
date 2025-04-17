54087b7b361423f34c6dd593ee14f058ab7d2a7b
Josh Birch <joshuabirch@protonmail.com>
2025-04-17 06:35:24 +0100

    pseudo

--- src/Problem001.java
+++ src/Problem001.java
@@ -1,3 +1,27 @@
+/*
+
+ Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances. For
+ example, if you pair up a 3 from the left list with a 7 from the right list, the distance apart is 4; if you pair up a
+ 9 with a 3, the distance apart is 6.
+
+In the example list above, the pairs and distances would be as follows:
+
+The smallest number in the left list is 1, and the smallest number in the right list is 3. The distance between them
+is 2.
+The second-smallest number in the left list is 2, and the second-smallest number in the right list is another 3. The
+distance between them is 1.
+The third-smallest number in both lists is 3, so the distance between them is 0.
+The next numbers to pair up are 3 and 4, a distance of 1.
+The fifth-smallest numbers in each list are 3 and 5, a distance of 2.
+Finally, the largest number in the left list is 4, while the largest number in the right list is 9; these are a
+distance 5 apart.
+To find the total distance between the left list and the right list, add up the distances between all of the pairs you
+found. In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
+
+Your actual left and right lists contain many location IDs. What is the total distance between your lists?
+
+*/
+
 import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -7,8 +31,8 @@ public class Problem001
     public static void main(String[] args)
     {
           /* EXAMPLE */
-//        int[] listOne = new int[]{3, 4, 2, 1, 3, 3};
-//        int[] listTwo = new int[]{4, 3, 5, 3, 9, 3};
+        int[] listOne = new int[]{};
+        int[] listTwo = new int[]{};
 //
 //        Arrays.sort(listOne);
 //        Arrays.sort(listTwo);
@@ -21,14 +45,19 @@ public class Problem001
 //        }
 //
 //        System.out.println(distance);
+    }
 
-        File numberFile = new File("problem001.dat");
+    public static void()
+    {
+        // while not end of file
+        // delimiter = " "
+        // counter = 0
 
-        try
-        {
-            Scanner fileScanner = new Scanner(numberFile);
+        // if counter = 0
+            //  add current number to array1
+            // if delimiter, counter + 1
 
-
-        }
+        // add current number to array2
+        // counter -1
     }
 }
